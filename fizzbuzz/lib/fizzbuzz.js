96ff99522c6b0068fc9f0171c8ab0b4238ca37c4
martincronje <martin@nreality.com>
2014-02-25 21:49:59 +0200

    Small change to make the implementation more elegant

--- fizzbuzz/lib/fizzbuzz.js
+++ fizzbuzz/lib/fizzbuzz.js
@@ -1,7 +1,5 @@
 'use strict';
 
-exports.FizzBuzz = function() {
-	this.evaluate = function(i) {
-		return (i%3 ? '':'Fizz') + (i%5 ? '':'Buzz') || i;
-	};
+exports.fizzbuzz = function(i) {
+	return (i%3 ? '':'Fizz') + (i%5 ? '':'Buzz') || i;
 };
