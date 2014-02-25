d3a685dd6242b3b6c3b8ff4cc604979544843ec6
martincronje <martin@nreality.com>
2014-02-25 22:17:22 +0200

    Setup project

---
+++ trains.ruby/spec/train_routes_spec.rb
@@ -0,0 +1,45 @@
+require 'train_routes'
+
+describe TrainRoutes do
+	it 'should return the distance of route A-B-C.' do
+		
+	end
+
+	it 'should return the distance of route A-D.' do
+
+	end
+
+	it 'should return the distance of route A-D-C.' do
+
+	end
+
+	it 'should return the distance route A-E-B-C-D.' do
+
+	end
+
+	it 'should return the distance route A-E-D.' do
+
+	end
+
+	it 'should return the number of trips starting at C and ending at C with a maximum of 3 stops.' do
+
+	end
+
+	it 'should return the number of trips starting at A and ending at C with exactly 4 stops.' do
+
+	end
+
+	it 'should calculate the length of the shortest route (in terms of distance to travel) from A to C.' do
+
+	end
+
+	it 'should calculate the length of the shortest route (in terms of distance to travel) from B to B.' do
+
+	end
+
+	it 'should calculate the number of different routes from C to C with a distance of less than 30.' do
+
+	end
+end
+
+
